82684b4207e9d7f6f9d69f864fbbe3d8def6400f
adams-awesome <40500536+adams-awesome@users.noreply.github.com>
2019-10-22 02:14:56 +0530

    Update Question2.java

--- java/src/main/java/skeleton/answers/Question2.java
+++ java/src/main/java/skeleton/answers/Question2.java
@@ -4,6 +4,31 @@ public class Question2 {
 
     public static int riskAndReward(int[] risk, int[] bonus, int[] trader) {
         // TODO Auto-generated method stub
-        return -5;
+        int bonus_value = 0;
+        int temp;
+        int n = bonus.length;
+        int i,j;
+        for (i = 0; i < n-1; i++) {
+			for (j = 0; j < n-i-1; j++) {
+    			if (bonus[j] > bonus[j+1]) {
+            		temp = bonus[j];
+            		bonus[j] = bonus[j+1];
+            		bonus[j+1] = temp;
+     				temp = risk[j];
+     				risk[j] = risk[j+1];
+     				risk[j+1] = temp;
+        		}
+    		}
+        }  
+
+        for (i = 0; i < trader.length; i++) {
+        	for (j = 0; j < n; j++) {
+        		if (trader[i] >= risk[j]) {
+        			bonus_value += bonus[j];
+        			break;
+        		}
+        	}
+        }
+    	return bonus_value;
     }
 }
